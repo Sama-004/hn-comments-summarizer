7dec9ed7dd3ec23ae7f87b4e5925c140e1754b52
sama-004 <70210929+Sama-004@users.noreply.github.com>
2024-06-26 21:32:34 +0530

    frontend changes

--- app/takeInput.tsx
+++ app/takeInput.tsx
@@ -1,25 +1,45 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import React from "react";
+import React, { useState } from "react";
 import { handleInput } from "./actions";
 
 function TakeInput() {
+  const [summary, setSummary] = useState("");
+  const [loading, setLoading] = useState(false);
   return (
-    <form
-      className="w-full flex items-center justify-center gap-4"
-      action={async (e) => {
-        const response = await handleInput(e);
-        console.log(response);
-      }}>
-      <Input
-        type="text"
-        placeholder="https://news.yocombinator.com/"
-        className="full max-w-2xl"
-        name="hnUrl"
-      />
-      <Button type="submit">Summarize</Button>
-    </form>
+    <div>
+      <form
+        className="w-full flex items-center justify-center gap-4 bg-black text-white"
+        action={async (e) => {
+          setSummary("");
+          const response = await handleInput(e);
+          console.log(response);
+          if (response) setSummary(response);
+        }}>
+        <Input
+          type="text"
+          placeholder="https://news.yocombinator.com/item?id=number"
+          className="full max-w-2xl bg-black text-white"
+          name="hnUrl"
+        />
+        <Button type="submit">Summarize</Button>
+      </form>
+
+      {summary ? (
+        <div className="w-full max-w-2xl bg-black text-white rounded-xl mt-10 border-10 border-white">
+          {summary}
+        </div>
+      ) : (
+        <div className="w-full max-w-2xl p-4 bg-black text-white rounded-xl mt-10 border-2 border-white">
+          {loading
+            ? `loading`
+            : `
+          use me to summarize the comments on a hacker news post so that you don't have to
+          `}
+        </div>
+      )}
+    </div>
   );
 }
 
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,7 @@
 import { zValidator } from "@hono/zod-validator";
-import axios from "axios";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { z } from "zod";
-import { isValid } from "zod";
 
 type Bindings = {
   [key in keyof CloudflareBindings]: CloudflareBindings[key];
@@ -12,48 +10,6 @@ type Bindings = {
 const app = new Hono<{ Bindings: Bindings }>();
 
 app.use("/*", cors());
-
-// app.get("/", async (c) => {
-//   const url = c.req.query("url");
-
-//   if (!url?.startsWith("https://news.ycombinator.com/item?id=")) {
-//     return c.json(
-//       {
-//         message: "Invalid URL",
-//       },
-//       401
-//     );
-//   }
-
-//   const fullJson = await axios.get(url);
-
-//   const comments = fullJson.data.match(
-//     /<div class="commtext c01">(.*?)<\/div>/gs
-//   );
-
-//   if (!comments) {
-//     return c.json(
-//       {
-//         message: "No comments found",
-//       },
-//       405
-//     );
-//   }
-
-//   return c.text(comments.json("\n"));
-//   const textComments = comments.map((comment: any) =>
-//     comment.replace(/<div class="commtext c01">|<\/div>/gs, "")
-//   );
-//   const topComments = textComments.slice(1, Math.min(textComments.length, 5));
-
-//   // console.log(topComments);
-
-//   const response = await c.env.AI.run("@cf/mistral/mistral-6b-instruct-v0.1", {
-//     prompt: `AI, please summarize the top 6 comments from the given HackerNews thread that is : ${topComments}. Provide a concise summary, capturing the main points and arguments made by the users. Ensure that the summaries are unbiased, coherent, and maintain the original context.`,
-//   });
-
-//   console.log(response);
-//   return c.json({ message: response }, 201);
 
 const isValidUrl = (value: string) => {
   if (!value.startsWith("https://news.ycombinator.com")) return false;
@@ -90,31 +46,11 @@ app.get(
 
     // console.log(commentsList);
 
-    let sentiment = "";
-    let accumSentiment = "";
-
     const resp = (await c.env.AI.run("@cf/mistral/mistral-7b-instruct-v0.1", {
-      prompt: `summarize this set of comments on hacker news(a website where programmers basically share their learning and what they are building), to basically give an overview of what the users think about the post: ${commentsList.join(
+      prompt: `summarize this set of comments which is from a website where programmers share each other ideas and projects they've built, to basically give an overview of what the users think about the post: ${commentsList.join(
         "\n"
       )} `,
     })) as { response: string };
-    // for (let i = 0; i < commentsList.length; i++) {
-    //   if (i % 10) {
-    //     console.log("Sentences:", accumSentiment);
-
-    //     const resp = (await c.env.AI.run(
-    //       "@cf/mistral/mistral-7b-instruct-v0.1",
-    //       {
-    //         prompt: `summarize this set of comments: ${accumSentiment}, but also, include this context when you do the summarization: ${sentiment}. be consise in your responses`,
-    //       }
-    //     )) as { response: string };
-    //     accumSentiment = "";
-    //     sentiment += resp.response;
-    //   } else {
-    //     accumSentiment += "\nNew comment " + commentsList[i];
-    //   }
-    // }
-    // return c.json({ sentiment });
     console.log(resp.response);
     return c.json({ response: resp.response });
   }
